1f0cf3aa2c1a68673c8927b92a28cc1ce2581f1e
maximmim <93773088+maximmim@users.noreply.github.com>
2022-06-29 14:10:30 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,17 +7,27 @@ from aiogram.utils.emoji import emojize
 
 import re
 import os
-
+import random
 from time import sleep
+import time
 
 
 import yt_download as yt
 import config 
 
 
-bot = Bot(token="5312847705:AAE0ii_TUhEeuNPRV52iiFmB0bsEInhANt4")
+def random_id():
+    return random.randint(0, 1000000)
+
+bot = Bot(config.TOKEN)
 URL = "https://painmo.herokuapp.com/"
 dp = Dispatcher(bot)
+
+b1 = KeyboardButton('Помощь')
+b2 = KeyboardButton('Скачять')
+
+kb_client = ReplyKeyboardMarkup()
+kb_client.add(b1).add(b2)
 
    
 @dp.message_handler(commands=['start'])
@@ -26,26 +36,40 @@ async def process_help_command(message: types.Message):
     await message.delete()
 
 
+
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
+    await message.reply(config.STARTEXT)
     await message.reply("Надішліть мені посилання на відео \n і я допоможу завантажити його")
     await message.delete()
 @dp.message_handler(content_types=['text'])
 async def echo_download_message(message: types.Message):
+    if message.text == 'start':
+            await message.reply("Привіт!\n Я Паймон і допоможу завантажити відео з будь-якого сайту якщо потрібна допомога пиши /help")
+    elif message.text == 'help':
+        await message.reply("Надішліть мені посилання на відео \n і я допоможу завантажити його")
+
     try:
         
         echo_download=yt.Downloader(message.text)
         await message.reply("Побачила, починаю закачку...")
+        time.sleep(2)
+        '''await message.reply("Генерирую id...")
+        time.sleep(4)
+        await message.reply(random_id())
+        '''
+        
         videonote = open(echo_download.download_video(), 'rb')
+     
     except:
         await message.reply("На жаль, сталася помилка... Перевірте правильність посилання")
         print('Error :(')
         return
-    await message.reply("Готово, відео завантажено на сервер.\nВідправляю...",)
+    await message.reply("Готово, видео зкачено на сервер.\nВідправляю...",)
     try:
         await bot.send_document(message.from_user.id, videonote)
     except:
-        await bot.send_message(message.fr om_user.id, "На жаль, сталася помилка...")
+        await bot.send_message(message.from_user.id, "На жаль, сталася помилка...")
     finally:
         videonote.close()
 
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN="2056524233:AAHpJ8L4lzRVobBLvo63aR6AV309_MwPko8"
+TOKEN="5312847705:AAE0ii_TUhEeuNPRV52iiFmB0bsEInhANt4"
 download_folder='user_videos'
+NAME = 'Paimon'
+STARTEXT = "Привіт я багато функціональний телеграм бот. \n Та звати мене Паймон"
